de8d7bc603df93b8b3a22f33cc56fd6057d7d5de
sigmade <sychyov1991@mail.ru>
2020-07-02 21:55:05 +0300

    Add project files.

---
+++ EfApi/Models/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using EfApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=ApiDB; Trusted_Connection=True");
+        }
+    }
+}
